0a6959da09294b8dfccde05573b1a8cb84cd6809
fareed <fareed@kono.ai>
2017-09-28 19:43:07 +0900

    - type script def - editor config

---
+++ lib/index.d.ts
@@ -0,0 +1,139 @@
+// Type definitions for react-animated-css 1.0.2
+// Project: https://github.com/digital-flowers/react-animated-css
+// <reference types="react" />
+import {Component, Ref} from "react";
+
+declare module "react-animated-css" {
+  type AnimationString =
+    "bounce"
+    | "flash"
+    | "pulse"
+    | "rubberBand"
+    | "shake"
+    | "headShake"
+    | "swing"
+    | "tada"
+    | "wobble"
+    | "jello"
+    | "bounceIn"
+    | "bounceInDown"
+    | "bounceInLeft"
+    | "bounceInRight"
+    | "bounceInUp"
+    | "bounceOut"
+    | "bounceOutDown"
+    | "bounceOutLeft"
+    | "bounceOutRight"
+    | "bounceOutUp"
+    | "fadeIn"
+    | "fadeInDown"
+    | "fadeInDownBig"
+    | "fadeInLeft"
+    | "fadeInLeftBig"
+    | "fadeInRight"
+    | "fadeInRightBig"
+    | "fadeInUp"
+    | "fadeInUpBig"
+    | "fadeOut"
+    | "fadeOutDown"
+    | "fadeOutDownBig"
+    | "fadeOutLeft"
+    | "fadeOutLeftBig"
+    | "fadeOutRight"
+    | "fadeOutRightBig"
+    | "fadeOutUp"
+    | "fadeOutUpBig"
+    | "flipInX"
+    | "flipInY"
+    | "flipOutX"
+    | "flipOutY"
+    | "lightSpeedIn"
+    | "lightSpeedOut"
+    | "rotateIn"
+    | "rotateInDownLeft"
+    | "rotateInDownRight"
+    | "rotateInUpLeft"
+    | "rotateInUpRight"
+    | "rotateOut"
+    | "rotateOutDownLeft"
+    | "rotateOutDownRight"
+    | "rotateOutUpLeft"
+    | "rotateOutUpRight"
+    | "hinge"
+    | "jackInTheBox"
+    | "rollIn"
+    | "rollOut"
+    | "zoomIn"
+    | "zoomInDown"
+    | "zoomInLeft"
+    | "zoomInRight"
+    | "zoomInUp"
+    | "zoomOut"
+    | "zoomOutDown"
+    | "zoomOutLeft"
+    | "zoomOutRight"
+    | "zoomOutUp"
+    | "slideInDown"
+    | "slideInLeft"
+    | "slideInRight"
+    | "slideInUp"
+    | "slideOutDown"
+    | "slideOutLeft"
+    | "slideOutRight"
+    | "slideOutUp";
+
+  export class Animated extends React.Component<AnimatedProps> {
+  }
+
+  export interface AnimatedProps {
+    /**
+     * The animation-in style.
+     * @default "fadeIn"
+     */
+    animationIn: AnimationString;
+
+    /**
+     * The animation-out style.
+     * @default "fadeOut"
+     */
+    animationOut: AnimationString;
+
+    /**
+     * The animation delay.
+     * @default 0
+     */
+    animationInDelay?: number;
+
+    /**
+     * The animation delay.
+     * @default 0
+     */
+    animationOutDelay?: number;
+
+    /**
+     * Additional CSS styling.
+     * @default {}
+     */
+    style?: object;
+
+    /**
+     * Is Animation visible?
+     * @default true
+     */
+    isVisible: boolean;
+
+    innerRef?: React.Ref<Animated>;
+
+    /**
+     * Additional class name.
+     * @default ""
+     */
+    className?: string;
+
+    /**
+     * Will it display animation on mounting?
+     * @default true
+     */
+    animateOnMount?: boolean;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import classNames from "classnames";
  * @return {boolean}
  */
 const isLteIE9 = () => {
-    const ua = window.navigator.userAgent;
-    const ie = ua.indexOf("MSIE ");
-    return ie > -1 && parseInt(ua.substring(ie + 5, ua.indexOf(".", ie))) <= 9;
+  const ua = window.navigator.userAgent;
+  const ie = ua.indexOf("MSIE ");
+  return ie > -1 && parseInt(ua.substring(ie + 5, ua.indexOf(".", ie))) <= 9;
 };
 
 /**
@@ -17,66 +17,66 @@ const isLteIE9 = () => {
  * @type {Object}
  */
 export class Animated extends React.Component {
-    timeoutId;
+  timeoutId;
 
-    constructor(props) {
-        super(props);
-        this.state = props.animateOnMount ? {
-            animation: props.isVisible ? props.animationIn : props.animationOut
-        } : {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = props.animateOnMount ? {
+      animation: props.isVisible ? props.animationIn : props.animationOut
+    } : {};
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const {isVisible} = nextProps;
-        if (isVisible !== this.props.isVisible) {
-            const {animationIn, animationOut} = this.props;
-            clearTimeout(this.timeoutId);
-            this.setState({animation: isVisible ? animationIn : animationOut});
-        }
+  componentWillReceiveProps(nextProps) {
+    const {isVisible} = nextProps;
+    if (isVisible !== this.props.isVisible) {
+      const {animationIn, animationOut} = this.props;
+      clearTimeout(this.timeoutId);
+      this.setState({animation: isVisible ? animationIn : animationOut});
     }
+  }
 
-    componentWillUnmount() {
-        clearTimeout(this.timeoutId);
-    }
+  componentWillUnmount() {
+    clearTimeout(this.timeoutId);
+  }
 
-    render() {
-        const {children, animationInDelay, animationOutDelay, style, isVisible, innerRef, className} = this.props;
-        const {animation} = this.state;
-        const classes = classNames("animated", animation, className);
-        if (isLteIE9() || !animation) {
-            style.opacity = isVisible ? 1 : 0;
-        }
-        return (
-            <div className={classes} ref={innerRef} style={{
-                animationDelay: `${isVisible ? animationInDelay : animationOutDelay}s`,
-                pointerEvents: isVisible ? "all" : "none",
-                ...style
-            }}>
-                {children}
-            </div>
-        );
+  render() {
+    const {children, animationInDelay, animationOutDelay, style, isVisible, innerRef, className} = this.props;
+    const {animation} = this.state;
+    const classes = classNames("animated", animation, className);
+    if (isLteIE9() || !animation) {
+      style.opacity = isVisible ? 1 : 0;
     }
+    return (
+      <div className={classes} ref={innerRef} style={{
+        animationDelay: `${isVisible ? animationInDelay : animationOutDelay}s`,
+        pointerEvents: isVisible ? "all" : "none",
+        ...style
+      }}>
+        {children}
+      </div>
+    );
+  }
 }
 
 Animated.propTypes = {
-    animationIn: PropTypes.string,
-    animationOut: PropTypes.string,
-    animationInDelay: PropTypes.number,
-    animationOutDelay: PropTypes.number,
-    style: PropTypes.object,
-    isVisible: PropTypes.bool,
-    innerRef: PropTypes.func,
-    className: PropTypes.string,
-    animateOnMount: PropTypes.bool
+  animationIn: PropTypes.string,
+  animationOut: PropTypes.string,
+  animationInDelay: PropTypes.number,
+  animationOutDelay: PropTypes.number,
+  style: PropTypes.object,
+  isVisible: PropTypes.bool,
+  innerRef: PropTypes.func,
+  className: PropTypes.string,
+  animateOnMount: PropTypes.bool
 };
 
 Animated.defaultProps = {
-    animationIn: "fadeIn",
-    animationOut: "fadeOut",
-    className: "",
-    animationInDelay: 0,
-    animationOutDelay: 0,
-    isVisible: true,
-    style: {},
-    animateOnMount: true
+  animationIn: "fadeIn",
+  animationOut: "fadeOut",
+  className: "",
+  animationInDelay: 0,
+  animationOutDelay: 0,
+  isVisible: true,
+  style: {},
+  animateOnMount: true
 };
